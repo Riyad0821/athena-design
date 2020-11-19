c1192495721ed0a40c0ffcd82ad1777b47658be3
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-11-19 16:54:06 +0600

    contant us component initial set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Service from './components/Home/Service/Service';
+import ContactUs from './components/Home/ContactUs/ContactUs';
 
 function App() {
   return (
     <div>
      <Service/>
+     <ContactUs/>
     </div>
   );
 }
